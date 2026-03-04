04bd823c7893df92f1afca805aac268f7e79164c
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-03-04 13:27:13 +0200

    Auto-update: 2026-03-04 13:27

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1772623574
+ItemStorageDB_LastUpdate = 1772623605
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -7712,7 +7712,7 @@ ItemStorageDB = {
     },
     {
         name = "Мясушко",
-        updated_at = "2026-03-04 12:54:21",
+        updated_at = "2026-03-04 13:26:44",
         money = 13874,
         empty = 0,
         location = "Оргриммар",
@@ -8344,7 +8344,7 @@ ItemStorageDB = {
             {
                 link = "|cffffffff|Hitem:5504:0:0:0:0:0:0:0:10|h[Пикантное мясо моллюска]|h|r",
                 name = "Пикантное мясо моллюска",
-                count = 38,
+                count = 43,
                 details = {
                     quality = 1,
                     type = "",
